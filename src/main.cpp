065e0eafc859c48edd24a04961cfef4f458f7230
trizer <mrtrizer@gmail.com>
2016-03-14 01:10:35 +0300

    All ready. But doesn't work yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <ctrl.h>
 #include <glut/glutmgr.h>
-#include <png/viewfactorylibpng.h>
+//#include <png/viewfactorylibpng.h>
+#include <sdl/viewfactorysdl.h>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    GLUTMgr::initGLUT(argc, argv, std::make_shared<ViewFactoryLibPNG>("./res/drawable-hdpi/"), std::make_shared<Ctrl>());
+    //GLUTMgr::initGLUT(argc, argv, std::make_shared<ViewFactoryLibPNG>("./res/drawable-hdpi/"), std::make_shared<Ctrl>());
+    GLUTMgr::initGLUT(argc, argv, std::make_shared<ViewFactorySDL>("./res/drawable-hdpi/"), std::make_shared<Ctrl>());
     return 0;
 }
